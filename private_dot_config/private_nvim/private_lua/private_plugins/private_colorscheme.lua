b818e76233c530149c16123314e25e7fbcca75d7
mlnchlc <abhishek0000mahajan@gmail.com>
2025-08-20 01:05:12 +0530

    catpuccin

--- private_dot_config/private_nvim/private_lua/private_plugins/private_colorscheme.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_colorscheme.lua
@@ -1,6 +1,35 @@
 return {
   { "mellow-theme/mellow.nvim" },
-  { "rebelot/kanagawa.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true, -- disables setting the background color.
+      default_integrations = true,
+      auto_integrations = true,
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      transparent = true,
+    },
+  },
   { "projekt0n/github-nvim-theme" },
   { "sainnhe/everforest" },
 
@@ -8,7 +37,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "catppuccin",
     },
   },
 }
